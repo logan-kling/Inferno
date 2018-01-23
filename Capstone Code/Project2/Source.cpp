c09176fa2333e5dbb02595d82f868dd4397b2f86
zaengled <zaengled@oregonstate.edu>
2018-01-23 12:42:48 -0800

    Adding UI Elements
    Added some text boxes for input contained in static boxes with borders

--- Capstone Code/Project2/Source.cpp
+++ Capstone Code/Project2/Source.cpp
@@ -1,15 +1,42 @@
+#include <wx/wx.h>
 #include <wx/app.h>
 #include <wx/frame.h>
 #include <wx/menu.h>
+#include <wx/panel.h>
+#include <wx/sizer.h>
+#include <wx/splitter.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
 
 class MinApp : public wxApp {
 public:
 	virtual bool OnInit();
 };
 
+class InputPanel : public wxPanel {
+public:
+	InputPanel(wxWindow * parent);
+private:
+	wxStaticText *i_test;
+	wxTextCtrl *i_v1, *i_v2, *i_v3, *i_v4;
+	wxSizer *i_sizer;
+};
+
+class OutputPanel : public wxPanel {
+public:
+	OutputPanel(wxWindow * parent);
+private:
+	wxStaticText *o_test;
+	wxSizer *o_sizer;
+};
+
 class MinFrame : public wxFrame {
 public:
 	MinFrame(const wxString& title);
+
+	InputPanel *in_p;
+	OutputPanel *out_p;
+
 private:
 	wxMenuBar *menuBar;
 	wxMenu *fileMenu;
@@ -48,10 +75,68 @@ MinFrame::MinFrame(const wxString& title)
 	menuBar->Append(helpMenu, "&Help");
 
 	SetMenuBar(menuBar);
+
+	// Menu bar is now done and placed
+
+	// This is for everything else
+
+	// Create a split window so we can have input and output side by side
+	wxSplitterWindow *splitter = new wxSplitterWindow(this, 
+		wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_NOBORDER);
+	in_p = new InputPanel(splitter);
+	out_p = new OutputPanel(splitter);
+	splitter->SplitVertically(in_p, out_p);
+	splitter->SetMinimumPaneSize(20);
 }
+
+
+// This is basically the 'main()' function, program starts here
 
 bool MinApp::OnInit() {
 	MinFrame* frame = new MinFrame("Basic");
 	frame->Show(true);
 	return true;
+}
+
+InputPanel::InputPanel(wxWindow * parent)
+	: wxPanel(parent, wxID_ANY){
+	
+	i_sizer = new wxStaticBoxSizer(wxVERTICAL, this, "Input");
+	i_sizer->Add(
+		new wxTextCtrl(this, -1, "Distance", wxDefaultPosition, wxDefaultSize, wxTE_LEFT),
+		0,            // not vertically stretchable
+		wxALL,        // and make border all around
+		10);          // set border width to 10
+	i_sizer->Add(
+	new wxTextCtrl(this, -1, "Charge", wxDefaultPosition, wxDefaultSize, wxTE_LEFT),
+		0,
+		wxALL,
+		10);
+	i_sizer->Add(
+		new wxTextCtrl(this, -1, "Motor Consumption", wxDefaultPosition, wxDefaultSize, wxTE_LEFT),
+		0,
+		wxALL,
+		10);
+
+	SetSizer(i_sizer);
+	i_sizer->SetSizeHints(this);
+}
+
+OutputPanel::OutputPanel(wxWindow * parent)
+	: wxPanel(parent, wxID_ANY) {
+
+	o_sizer = new wxStaticBoxSizer(wxVERTICAL, this, "Output");
+	o_sizer->Add(
+		new wxTextCtrl(this, -1, "Output", wxDefaultPosition, wxDefaultSize, wxTE_LEFT),
+		0,
+		wxALL,
+		10);
+	o_sizer->Add(
+		new wxTextCtrl(this, -1, "Graph will go here", wxDefaultPosition, wxDefaultSize, wxTE_LEFT),
+		1,				// Yes stretch
+		wxALL,
+		10);
+	
+	SetSizer(o_sizer);
+	o_sizer->SetSizeHints(this);
 }
