653a190e7948da2e5808e849e3d117d654c93b76
Zaengled <zaengled@oregonstate.edu>
2018-04-25 09:28:33 -0700

    Adding more tests
    tests for our conversion functions and the get distance function

--- Capstone Code/UnitTest1/unittest1.cpp
+++ Capstone Code/UnitTest1/unittest1.cpp
@@ -21,3 +21,39 @@ namespace UnitTest1
 
 	};
 }
+
+namespace getDorSTest 
+{
+	TEST_CLASS(dTest) 
+	{
+	public:
+
+		TEST_METHOD(DistanceTest1) {
+			float distance_result = 0.0f;
+			float test_equal = 0.0f;
+			test_equal = ((1000.0f * 1.0f * log2((float)1.0f / 8.0f - (float)1.0f / 28.0f + 1.0f)) / (1.0f * (1.0f/50.0f + 1.0f) * log(1.0f + 1.0f) + 1.0f * 1.0f));
+			distance_result = getDistance(1, 1, 1, 1, 1, 1);
+			Assert::AreEqual(distance_result, test_equal);
+		}
+	};
+}
+
+namespace conversionTests
+{
+	TEST_CLASS(conversions)
+	{
+	public:
+
+		TEST_METHOD(HpToWattTest) {
+			double func_result = HpToWatt(1);
+			double test_equal = (double)1 * (double)745.699872;
+			Assert::AreEqual(func_result, test_equal);
+		}
+
+		TEST_METHOD(WattToHpTest) {
+			double func_result = WattToHp(1);
+			double test_equal = (double)1 * (double)0.00134102;
+			Assert::AreEqual(func_result, test_equal);
+		}
+	};
+}
